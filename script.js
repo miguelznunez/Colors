e7391361e5bfbda690717054517c06dda6f6ecb5
Miguel Nunez <71807189+iamcodefoxx@users.noreply.github.com>
2021-07-30 09:31:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
+var colors = ["Orangered","Blue","Yellow","Green","Orange", "Cyan","DodgerBlue", "Violet", "Navy", "Purple", "YellowGreen", "OrangeRed", "SlateBlue", "Salmon", "Crimson", "HotPink", "Magenta"];
 var index = 0;
 
-function changeColor(){
-  var colors = ["Red","Blue","Yellow","Green","Orange", "Cyan","DodgerBlue", "Violet", "Navy", "Purple", "YellowGreen", "OrangeRed", "SlateBlue", "Salmon", "Crimson", "HotPink", "Magenta"];
-  
-  document.getElementsByTagName("body")[0].style.background = colors[index++];
-
+document.querySelector("#submit").addEventListener("click", () => {
   if(index > colors.length - 1)
     index = 0;
-}
+  document.body.style.backgroundColor = colors[index++];  
+});
